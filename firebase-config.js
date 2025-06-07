1cd60ec6685f14d3bcc8ef9bb1ff7571457e9a9d
Afaka200998 <200998faka@gmail.com>
2025-06-07 19:28:53 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+
+// firebase-config.js
+// Replace these values with your Firebase project settings
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC7EXAMPLE",
+  authDomain: "mi-vault.firebaseapp.com",
+  projectId: "mi-vault",
+  storageBucket: "mi-vault.appspot.com",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:web:abcdefg1234567"
+};
+
+export default firebaseConfig;
